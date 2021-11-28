9ba2346f10f6937c2daa5f5c45e396903a131eed
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2021-11-28 10:46:10 +0100

    test

--- src/archiver.rs
+++ src/archiver.rs
@@ -101,3 +101,118 @@ pub fn create_mail_archiver(mode: ArchiveMode) -> Box<dyn MaildirArchiver> {
         ArchiveMode::Copy => Box::new(CopyMaildirArchiver {}),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use maildir::Maildir;
+
+    struct MaildirRaii {
+        basedir: PathBuf,
+        input_maildir: Maildir,
+        output_maildir: Maildir,
+    }
+
+    impl MaildirRaii {
+        fn new() -> Self {
+            use mktemp::Temp;
+            let basedir = Temp::new_dir().unwrap().to_path_buf();
+            println!("{}", basedir.display());
+            let input_maildir = Maildir::from(basedir.join("in"));
+            let output_maildir = Maildir::from(basedir.join("out"));
+            input_maildir.create_dirs().unwrap();
+            output_maildir.create_dirs().unwrap();
+
+            let filename = "1463868505.38518452d49213cb409aa1db32f53184:2,S";
+            std::fs::copy(
+                format!("testdata/maildir1/cur/{}", filename),
+                input_maildir.path().join("cur").join(filename),
+            ).unwrap();
+
+            MaildirRaii {
+                basedir: basedir,
+                input_maildir: input_maildir,
+                output_maildir: output_maildir,
+            }
+        }
+    }
+
+    impl Drop for MaildirRaii {
+        fn drop(&mut self) {
+            std::fs::remove_dir_all(
+                &self.basedir
+            ).unwrap();
+        }
+    }
+
+    #[test]
+    fn test_move_archive_email() {
+        use crate::archiver::MoveMaildirArchiver;
+        use crate::archiver::MaildirArchiver;
+
+        let maildir = MaildirRaii::new();
+        let archiver = MoveMaildirArchiver {};
+        let mail = match maildir.input_maildir.list_cur().next() {
+            Some(Ok(m)) => m,
+            _ => panic!("Something terrible"),
+        };
+
+        assert_eq!(maildir.input_maildir.count_cur(), 1);
+        assert_eq!(maildir.output_maildir.count_cur(), 0);
+        archiver.archive_email(
+            &mail,
+            &maildir.input_maildir,
+            &maildir.output_maildir,
+        ).unwrap();
+        assert_eq!(maildir.input_maildir.count_cur(), 0);
+        assert_eq!(maildir.output_maildir.count_cur(), 1);
+    }
+
+    #[test]
+    fn test_copy_archive_email() {
+        use crate::archiver::CopyMaildirArchiver;
+        use crate::archiver::MaildirArchiver;
+
+        let maildir = MaildirRaii::new();
+        let archiver = CopyMaildirArchiver {};
+        let mail = match maildir.input_maildir.list_cur().next() {
+            Some(Ok(m)) => m,
+            _ => panic!("Something terrible"),
+        };
+
+        assert_eq!(maildir.input_maildir.count_cur(), 1);
+        assert_eq!(maildir.output_maildir.count_cur(), 0);
+        archiver.archive_email(
+            &mail,
+            &maildir.input_maildir,
+            &maildir.output_maildir,
+        ).unwrap();
+        assert_eq!(maildir.input_maildir.count_cur(), 1);
+        assert_eq!(maildir.output_maildir.count_cur(), 1);
+    }
+
+    #[test]
+    fn test_dryrun_archive_email() {
+        use crate::archiver::DryRunMaildirArchiver;
+        use crate::archiver::MaildirArchiver;
+
+        let maildir = MaildirRaii::new();
+        let archiver = DryRunMaildirArchiver {};
+        let mail = match maildir.input_maildir.list_cur().next() {
+            Some(Ok(m)) => m,
+            _ => panic!("Something terrible"),
+        };
+
+        assert_eq!(maildir.input_maildir.count_cur(), 1);
+        assert_eq!(maildir.output_maildir.count_cur(), 0);
+        archiver.archive_email(
+            &mail,
+            &maildir.input_maildir,
+            &maildir.output_maildir,
+        ).unwrap();
+        assert_eq!(maildir.input_maildir.count_cur(), 1);
+        assert_eq!(maildir.output_maildir.count_cur(), 0);
+    }
+}
+
+
